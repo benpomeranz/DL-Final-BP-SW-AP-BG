aa88aa01b927bc550894695aacf227e16212b30d
benpomeranz <benjamin_pomeranz@brown.edu>
2024-04-19 17:44:48 -0400

    oh boy

--- DataTesting.py
+++ DataTesting.py
@@ -1,11 +1,44 @@
-from openeew.data.aws import AwsDataClient
+import json 
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import ast
 
-data_client = AwsDataClient('mx')
+def process_file(file_path):
+    data = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            json_object = json.loads(line)
+            data.append(json_object["x"])
+    return data
 
-start_date_utc = '2018-02-16 23:39:00'
-end_date_utc = '2018-02-16 23:42:00'
+data_list = []
+for root, dirs, files in os.walk('2018'):
+    for file in files:
+        if file.endswith('.jsonl'):
+            file_path = os.path.join(root, file)
+            data = process_file(file_path)
+            data_list.append(data)
 
-records = data_client.get_filtered_records(
-  start_date_utc,
-  end_date_utc
-  )
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('my_data.csv')
+
+# Define a function to parse strings into lists
+def parse_list(s):
+    if pd.isna(s):
+        return []
+    else:
+        return ast.literal_eval(s)
+
+# Apply the function to each element in the DataFrame
+df = df.applymap(parse_list)
+
+df = df.applymap(pd.Series)
+
+# Plot the first two series
+df.iloc[0].plot(label='Period 1')
+df.iloc[1].plot(label='Period 2')
+# Add more lines as needed
+plt.legend()
+plt.show()
