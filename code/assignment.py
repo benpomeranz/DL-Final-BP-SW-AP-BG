c442b8e718fe1e8f8dec878c044022e0116d3f52
bgitig <bentzion_gitig@brown.edu>
2024-04-30 22:14:17 -0400

    s

--- code/assignment.py
+++ code/assignment.py
@@ -10,4 +10,12 @@ from tensorflow.math import sigmoid
 from tqdm import tqdm
 
 from model import TPPModel
+# nll takes in list of distributions, input to call
 
+def train(model, data, has_accel):
+    for magnitude, times, accels in data:
+        with tf.GradientTape() as tape:
+            pred = model.call(magnitude, times, accels, has_accel, training=True)
+            loss = model.nll(pred, has_accel)
+        grads = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(grads, model.trainable_variables))
